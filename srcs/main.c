45976610294af77ab358bafe1bc4d916ddf78828
ezalos <ezalos@gmail.com>
2020-05-24 20:49:40 +0200

    ez pz cp paste

--- srcs/main.c
+++ srcs/main.c
@@ -5,9 +5,14 @@ int main(int ac, char **av)
 	t_arena		arena;
 	
 	init_board(&arena);
-	if (ac > 1)
-		ft_printf("%s\n", av[1]);
-
-	get_input(&arena);
+	(void)ac;
+	(void)av;
+	while (1)
+	{
+		get_input(&arena);
+		((t_coor*)arena.snake->body->content)->row += arena.move[SNK_ROW];
+		((t_coor*)arena.snake->body->content)->col += arena.move[SNK_COL];
+		snk_print(&arena);
+	}
 	return (0);
 }
